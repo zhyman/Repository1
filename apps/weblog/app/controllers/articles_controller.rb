e9dd493be2fe227ba157f250a2f963ec35156698
zhyman <zhyman@umich.edu>
2015-05-27 10:09:49 +0200

    d2 app update

--- apps/weblog/app/controllers/articles_controller.rb
+++ apps/weblog/app/controllers/articles_controller.rb
@@ -4,9 +4,17 @@ class ArticlesController < ApplicationController
 	end
 	
 	def new
+		@article = Article.new
 	end
 
 	def create
-	render plain: params['article'].inspect
+		@article = Article.new(article_params)
+
+		@article.save 
+		redirect_to @article
+	end
+
+	def article_params
+		params.require(:article).permit(:title, :text)
 	end
 end
