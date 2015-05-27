27f9b5ca53c587a80773e3a9ca6c55c110036ec7
zhyman <zhyman@umich.edu>
2015-05-27 14:31:22 +0200

    Added places controller

--- apps/lekker_class1/db/seeds.rb
+++ apps/lekker_class1/db/seeds.rb
@@ -7,5 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Place.delete_all
 
-Place.create! name: "Lion's Head", description: "Really tall awesome and scary"
-Place.create! name: "Table Mountain", description: "Even taller and more scary"
+Place.create! name: "Ace's of Spades", description: "Bar", address: "789 Bree St."
+Place.create! name: "Lion's Head", description: "Really tall awesome and scary", address: "123 Kloof St."
+Place.create! name: "Table Mountain", description: "Even taller and more scary", address: "456 Orange St."
