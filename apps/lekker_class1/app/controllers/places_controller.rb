bd39de726b51c19fb1f8e10f549546a9311a4a72
zhyman <zhyman@umich.edu>
2015-05-28 12:42:57 +0200

    lekker_plekke update

--- apps/lekker_class1/app/controllers/places_controller.rb
+++ apps/lekker_class1/app/controllers/places_controller.rb
@@ -20,6 +20,27 @@ class PlacesController < ApplicationController
 		end
 	end
 
+	def edit
+	@place = Place.find(params[:id])
+	end
+
+	def update
+		@place = Place.find(params[:id])
+
+		if @place.update place_params
+			redirect_to @place
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@place = Place.find(params[:id])
+
+		@place.destroy
+		redirect_to places_path
+	end
+
 private
 	
 	def place_params
